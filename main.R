746460aee79f2a7c4c6d2d76322be05abe277ebd
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-11-21 18:19:49 -0500

    rename 2 vars and call them factors from beginning

--- main.R
+++ main.R
@@ -1,7 +1,7 @@
 bdd = read.csv("~/Desktop/birthday_data/birthday data.csv", sep="|", colClasses=c("integer", "integer", "character", "character"))
 bdd$Time = bdd$hour + (bdd$min / 60)
 bdd$Length = nchar(bdd$message)
-bdd$Typical = (substr(bdd$message, 1, 5) ==  "Happy")
+bdd$Starts_with_word_Happy = (substr(bdd$message, 1, 5) ==  "Happy")
 
 count_chars = function(char, str) {
 	length(grep(char, strsplit(str, "")[[1]]))
@@ -11,7 +11,7 @@ count_exclam = function(str){
 	count_chars("!", str)
 }
 
-bdd$ExclamationPoints = apply(array(bdd$message), 1, count_exclam)
+bdd$Number_of_exclamation_points = factor(apply(array(bdd$message), 1, count_exclam))
 
  #################################
 
@@ -26,19 +26,19 @@ ggplot(data=bdd, aes(x=Time, y=Length)) + geom_density2d() + geom_point()
 
 # categorical vs numeric
 
-ggplot(data=bdd, aes(factor(ExclamationPoints), Time)) + geom_boxplot() + geom_jitter() + coord_flip()
-ggplot(data=bdd, aes(factor(ExclamationPoints), Length)) + geom_boxplot() + geom_jitter()
-ggplot(data=bdd, aes(factor(Typical), Length)) + geom_boxplot() + geom_jitter()
-ggplot(data=bdd, aes(factor(Typical), Time)) + geom_boxplot() + geom_jitter()
+ggplot(data=bdd, aes(Number_of_exclamation_points, Time)) + geom_boxplot() + geom_jitter() + coord_flip()
+ggplot(data=bdd, aes(Number_of_exclamation_points, Length)) + geom_boxplot() + geom_jitter()
+ggplot(data=bdd, aes(Starts_with_word_Happy, Length)) + geom_boxplot() + geom_jitter()
+ggplot(data=bdd, aes(Starts_with_word_Happy, Time)) + geom_boxplot() + geom_jitter()
 
-kruskal.test(bdd$Time, factor(bdd$ExclamationPoints))
-kruskal.test(bdd$Length, factor(bdd$ExclamationPoints))
-kruskal.test(bdd$Time, factor(bdd$Typical))
-kruskal.test(bdd$Length, factor(bdd$Typical)) # p = 0.0182. Ones that don't start with "Happy" are clearly longer.
+kruskal.test(bdd$Time, bdd$Number_of_exclamation_points)
+kruskal.test(bdd$Length, bdd$ExclamationPoints)
+kruskal.test(bdd$Time, bdd$Starts_with_word_Happy)
+kruskal.test(bdd$Length, bdd$Starts_with_word_Happy) # p = 0.0182. Ones that don't start with "Happy" are clearly longer.
 
 # category vs category
 
-table(bdd$Typical, bdd$ExclamationPoints)
-fisher.test(bdd$Typical, bdd$ExclamationPoints) # p-value = 0.04682
+table(bdd$Typical, bdd$Number_of_exclamation_points)
+fisher.test(bdd$Starts_with_word_Happy, bdd$Number_of_exclamation_points) # p-value = 0.04682
 
 # future thoughts: capitalization and presence of Birthday, whether it includes Andy, comma after birthday, category of person
