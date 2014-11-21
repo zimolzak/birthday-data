61e1f8d1775960e9aa22eba626ab019b06e80513
Andrew Zimolzak <andyzimolzak@gmail.com>
2014-11-21 18:08:28 -0500

    better univar, coord flip, plus update p vals

--- main.R
+++ main.R
@@ -18,14 +18,15 @@ bdd$ExclamationPoints = apply(array(bdd$message), 1, count_exclam)
 require(ggplot2)
 
 # univar
-ggplot(data=bdd, aes(x=Length)) + geom_histogram() + scale_x_log10()
+ggplot(data=bdd, aes(x=Length)) + geom_density() + scale_x_log10()
+ggplot(data=bdd, aes(x=Time)) + geom_density()
 
 # time series with some clustering
 ggplot(data=bdd, aes(x=Time, y=Length)) + geom_density2d() + geom_point()
 
 # categorical vs numeric
 
-ggplot(data=bdd, aes(factor(ExclamationPoints), Time)) + geom_boxplot() + geom_jitter()
+ggplot(data=bdd, aes(factor(ExclamationPoints), Time)) + geom_boxplot() + geom_jitter() + coord_flip()
 ggplot(data=bdd, aes(factor(ExclamationPoints), Length)) + geom_boxplot() + geom_jitter()
 ggplot(data=bdd, aes(factor(Typical), Length)) + geom_boxplot() + geom_jitter()
 ggplot(data=bdd, aes(factor(Typical), Time)) + geom_boxplot() + geom_jitter()
@@ -33,11 +34,11 @@ ggplot(data=bdd, aes(factor(Typical), Time)) + geom_boxplot() + geom_jitter()
 kruskal.test(bdd$Time, factor(bdd$ExclamationPoints))
 kruskal.test(bdd$Length, factor(bdd$ExclamationPoints))
 kruskal.test(bdd$Time, factor(bdd$Typical))
-kruskal.test(bdd$Length, factor(bdd$Typical)) # p = 0.02674
+kruskal.test(bdd$Length, factor(bdd$Typical)) # p = 0.0182. Ones that don't start with "Happy" are clearly longer.
 
 # category vs category
 
 table(bdd$Typical, bdd$ExclamationPoints)
-fisher.test(bdd$Typical, bdd$ExclamationPoints) # p-value = 0.1073
+fisher.test(bdd$Typical, bdd$ExclamationPoints) # p-value = 0.04682
 
 # future thoughts: capitalization and presence of Birthday, whether it includes Andy, comma after birthday, category of person
